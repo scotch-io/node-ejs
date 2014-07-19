fac8325cee3556e72b008f39b40a12e2585dda25
Chris Sevilleja <chris@scotch.io>
2014-07-19 10:59:50 -0700

    doing the templating thing

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('pages/index');
+});
+
+app.listen(8080);
+console.log('8080 is the magic port');
