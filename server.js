e529335b61d4b4a052e4e38684a0f4b53a7d5375
Chris Sevilleja <chris@scotch.io>
2014-07-27 17:36:51 -0700

    adding new stuff

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+// load the things we need
 var express = require('express');
 var app = express();
 
+// set the view engine to ejs
 app.set('view engine', 'ejs');
 
+// use res.render to load up an ejs view file
+
+// index page 
 app.get('/', function(req, res) {
 	res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+	res.render('pages/about');
 });
 
 app.listen(8080);
